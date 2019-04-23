54e5f1db53868421dc29c1d79eabe474606d76d5
K M Lawrence <keith@kludge.co.uk>
2019-04-23 11:46:05 +0100

    LACR-25 add certainty field to Search

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_02_13_221951) do
+ActiveRecord::Schema.define(version: 2019_04_23_100655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2017_02_13_221951) do
     t.string "date_incorrect"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "date_certainty", default: "high"
     t.index ["page"], name: "index_searches_on_page"
     t.index ["paragraph"], name: "index_searches_on_paragraph"
     t.index ["volume"], name: "index_searches_on_volume"

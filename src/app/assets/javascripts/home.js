267e21beed5f4d2f50d47c0825fd1619a32c7d21
K M Lawrence <keith@kludge.co.uk>
2019-04-24 12:44:51 +0100

    LACR-43 Update date format in search picker to UK format, controller to handle

--- src/app/assets/javascripts/home.js
+++ src/app/assets/javascripts/home.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  var dateFormat = 'yy-mm-dd';
-  var minDate = '1398-01-01';
-  var maxDate = '1511-12-31';
+  var dateFormat = 'dd/mm/yy';
+  var minDate = '01/01/1398';
+  var maxDate = '31/12/1511';
   // Initialise date fields for Advanced Search
   $( "#date_from" ).datepicker({
     showOtherMonths: true,
     selectOtherMonths: true,
-    dateFormat: 'yy-mm-dd',
+    dateFormat: dateFormat,
     minDate: $.datepicker.parseDate( dateFormat, minDate ),
     maxDate: $.datepicker.parseDate( dateFormat, maxDate ),
     defaultDate: $.datepicker.parseDate( dateFormat, minDate )

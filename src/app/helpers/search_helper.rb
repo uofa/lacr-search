af1b85c449ab1d4ccadda8cf0e5580b891904b7f
K M Lawrence <keith@kludge.co.uk>
2019-04-25 14:20:49 +0100

    LACR-32 show other date ranges in search results

--- src/app/helpers/search_helper.rb
+++ src/app/helpers/search_helper.rb
@@ -1,2 +1,11 @@
 module SearchHelper
+  def date_block_search(document)
+    if document.date_incorrect
+      document.date_incorrect
+    elsif document.date_not_after
+      "between #{document.date} and #{document.date_not_after}"
+    else
+      document.date
+    end
+  end
 end

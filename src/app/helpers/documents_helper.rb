8e554fd2f007bc2675fcb87ee3916abe6ea94520
K M Lawrence <keith@kludge.co.uk>
2019-05-03 17:42:01 +0100

    LACR-25 add class for between items, handle them differently if image present

--- src/app/helpers/documents_helper.rb
+++ src/app/helpers/documents_helper.rb
@@ -6,6 +6,9 @@ module DocumentsHelper
     if document.date_incorrect
       date_str += document.date_incorrect
     elsif document.date_not_after
+      # reset output to add helper class
+      date_str = '<strong>Date: </strong>'
+      date_str += "<span class=\"between cert-#{document.date_certainty}\">"
       date_str += "between #{document.date} and #{document.date_not_after}"
     else
       date_str += "#{document.date}"
